ecdc36ee1055942e42f8c59bcfb16053b9edca39
Zander Jimenez <zanderjimenez@Zanders-MacBook-Pro.local>
2021-02-24 17:55:45 -0800

    section 3 complete

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,15 @@ void main() => runApp(
       MaterialApp(
         debugShowCheckedModeBanner: false,
         home: Scaffold(
+          backgroundColor: Colors.blueGrey,
           appBar: AppBar(
             title: Text('I Am Rich'),
+            backgroundColor: Colors.deepPurple[600],
+          ),
+          body: Center(
+            child: Image(
+              image: AssetImage('images/diamond.png'),
+            ),
           ),
         ),
       ),
